edbe7503d8c68abdc3198a5851a545091bbebed6
Ryan <coap.ryanmontgomery@gmail.com>
2024-09-02 10:16:22 -0600

    completed 9-2

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -12,3 +13,16 @@ class Customer(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'Customer: {self.name}, age: {self.age}, email: {self.email}'
+
+class EmailAddress(db.Model):
+    __tablename__ = 'emailaddress'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    backup_email = db.Column(db.String)
+
+    @validates('email', 'backup_email')
+    def validate_email(self, key, address):
+        if '@' not in address:
+            raise ValueError("Failed simple email validation")
+        return address
